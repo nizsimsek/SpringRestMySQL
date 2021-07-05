f38540438b9dfbf345f818077a648912a0a9fa41
nizsimsek <nizsimsek@icloud.com>
2021-07-06 01:35:50 +0300

    ..

---
+++ src/main/java/com/nizsimsek/springrest/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.nizsimsek.springrest.controller;
+
+import com.nizsimsek.springrest.domain.Product;
+import com.nizsimsek.springrest.repository.ProductRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RepositoryRestResource
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.repository = productRepository;
+    }
+
+    @GetMapping
+    List<Product> getProducts () {
+        return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    Optional<Product> getById(@PathVariable Long id) {
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    Product addProduct(Product product) {
+        return repository.save(product);
+    }
+
+    @DeleteMapping("/{product}")
+    void deleteProduct(@PathVariable Product product) {
+        repository.delete(product);
+    }
+}
---
+++ src/main/java/com/nizsimsek/springrest/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.nizsimsek.springrest.repository;
+
+import com.nizsimsek.springrest.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
